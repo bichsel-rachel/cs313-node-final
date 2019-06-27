017414e59b8476472eee0ccd2c6ba66630788ad1
Rachel Bichsel <rachbichsel@gmail.com>
2019-06-27 13:42:44 -0600

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('views', __dirname + '/views');
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 //app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/form.html')))
 
@@ -50,18 +50,19 @@ app.get('/login', function(req, res) {
 app.get('/getCategory', function(req, res) {
    console.log("Worked");
    let id = req.query.id;
+   console.log(id);
 
  	getCategory(id, (error, result) => {
     if (error || result == null) {
       res.status(500).json({success: false, data: error});
     }else{
       res.json(result[0]);
-      let id = result[0].id;
-      let fname = result[0].first_name;
-      let tip = result[0].tip_title;
-      let description = result[0].tip_description;
-      let params = {id: id, fname: fname, tip: tip, description: description };
-      console.log(params);
+      // let id = result[0].id;
+      // let fname = result[0].first_name;
+      // let tip = result[0].tip_title;
+      // let description = result[0].tip_description;
+      // let params = {id: id, fname: fname, tip: tip, description: description };
+      // console.log(params);
     }
   });
 });
@@ -70,7 +71,7 @@ app.get('/getCategory', function(req, res) {
 function getCategory(id, callback) {
     // similar to PDO in PHP, $1::int gets back first data piece cleanly
     //let sql = 'SELECT * FROM tip WHERE id = $1::int';
-    let sql = 'SELECT * FROM category c INNER JOIN tip t ON c.id = t.category_id INNER JOIN users u ON t.users_id = u.id WHERE c.id = 1';
+    let sql = 'SELECT * FROM category c INNER JOIN tip t ON c.id = t.category_id INNER JOIN users u ON t.users_id = u.id WHERE c.id = $1::int';
     let params = [id];
     pool.query(sql, params, (err, result) => {
       if (err) {
@@ -78,7 +79,7 @@ function getCategory(id, callback) {
         console.log(err);
         callback(err,null);
       }
-      // console.log('found db result: ' + JSON.stringify(result.rows[0].fname));
+      console.log('found db result: ' + JSON.stringify(result.rows[0]));
       callback(null, result.rows);
     });
   };
